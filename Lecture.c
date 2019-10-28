c10fbe459776a742e66ba655dd4b17264a4b9365
agegout <adrien.gegout@gmail.com>
2019-10-28 17:30:24 +0100

    Optimizing read line number

--- Lecture.c
+++ Lecture.c
@@ -6,6 +6,9 @@
 #include <time.h>
 #include <sys/time.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/statfs.h>
 
 static int compare (const void *a, const void *b) 
 { 
@@ -32,7 +35,7 @@ int main (int argc, char *argv[]){
     FILE *fw;
 
     char * line_void = NULL;
-    volatile int lines = 2;
+    volatile int lines = 0;
     size_t len = 0;
     volatile double t1;
     volatile double t2;
@@ -45,23 +48,48 @@ int main (int argc, char *argv[]){
     int coverage = getline(&line_void, &len, fp);
     t2 = getTime();
     printf (" - time: %1.2lf sec\n",t2-t1);
+    fclose(fp);
 
     // Get lines number
     printf("Analysing line number ...\n");
     t1 = getTime();
-    int ch = 0;
-    while(!feof(fp)){
-        ch = fgetc(fp);
-        if(ch == '\n'){
-            lines++;
+    fp = fopen("reads.fasta", "r");
+    struct statfs fsInfo = {0};
+    int fd = fileno(fp); 
+    long optimalSize;
+
+    struct stat buf;
+    fstat(fd, &buf);
+    long file_size = buf.st_size;
+
+    if (fstatfs(fd, &fsInfo) == -1) {
+        optimalSize = 4 * 1024 * 1024;
+    } else {
+        optimalSize = fsInfo.f_bsize;
+    }
+
+    volatile long size_counter = 0;
+    char *BUFFER_SECONDARY = malloc(optimalSize);
+
+    while(size_counter < file_size){
+        fread(BUFFER_SECONDARY, optimalSize, 1,fp);
+        size_counter = size_counter + optimalSize;
+        for(int i = 0; i < optimalSize ; i++){
+            if(BUFFER_SECONDARY[i] == '\n'){
+                lines++;
+            }
         }
     }
+
+
+    free(BUFFER_SECONDARY);
     fclose(fp);
     t2 = getTime();
+    //printf ("%d\n",lines);
     printf (" - time: %1.2lf sec\n",t2-t1);
 
     // Regularizing values and creating BUFFER
-    printf("Creating Buffer ...\n");
+    printf("Creating primary Buffer ...\n");
     t1 = getTime();
     int MAX_SIZE = lines/2;
     
@@ -96,6 +124,9 @@ int main (int argc, char *argv[]){
     printf (" - time: %1.2lf sec\n",t2-t1);
 
     // Deleting duplicate
+    char **BUFFER_TWO = malloc(MAX_SIZE * sizeof(char*));
+
+    free(BUFFER_TWO);
 
     // Writing file
     printf("Writing file ...\n");
